fc3aaef3333a543796a99110dea7ccad4305ae8e
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-29 20:44:49 -0600

    Read optional trainer data

--- src/nes/cartridge.rs
+++ src/nes/cartridge.rs
@@ -6,7 +6,7 @@ use std::{
 
 use tock_registers::interfaces::Readable;
 
-use super::ines::INESHeader;
+use super::ines::{Flags1, INESHeader};
 
 pub struct Cartridge {
     header: INESHeader,
@@ -15,6 +15,9 @@ pub struct Cartridge {
 
 impl Cartridge {
     const VALID_MAGIC: [u8; 4] = [0x4E, 0x45, 0x53, 0x1A];
+    const HEADER_SIZE: u8 = 16;
+    const PRG_ROM_BLOCK_SZ: u16 = 16384;
+    const CHR_ROM_BLOCK_SZ: u16 = 8192;
 
     pub fn new(path: &str) -> Result<Self, Error> {
         // Open the ROM file
@@ -38,11 +41,17 @@ impl Cartridge {
         if header.chr_rom_size == 0 {
             return Err(Error::from(ErrorKind::Unsupported));
         }
+        // Skip the rest of the header
+        file.seek(SeekFrom::Start(Cartridge::HEADER_SIZE.into()))?;
+        // read trainer, if it exists
+        let mut trainer = None;
+        if header.flags1.is_set(Flags1::HAS_TRAINER) {
+            let mut trainer_data = [0u8; 512];
+            file.read_exact(&mut trainer_data)?;
+            trainer = Some(trainer_data);
+        }
 
-        Ok(Self {
-            header,
-            trainer: None,
-        })
+        Ok(Self { header, trainer })
     }
 }
 
