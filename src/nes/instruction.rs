93984a81622e8f87212b789e080f3e3074b7b707
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-12-30 19:31:47 -0600

    six new cpu instructions

--- src/nes/instruction.rs
+++ src/nes/instruction.rs
@@ -2,11 +2,17 @@ use std::fmt::Display;
 use std::io::Write;
 
 use bitfield::Bit;
-use tock_registers::interfaces::{ReadWriteable, Readable};
+use tock_registers::{
+    fields::Field,
+    interfaces::{ReadWriteable, Readable},
+};
 
 use super::{
     bus::Bus,
-    cpu::{Status, CPU},
+    cpu::{
+        Status::{self, Register},
+        CPU,
+    },
 };
 
 pub enum Operand {
@@ -41,6 +47,10 @@ impl CPU {
         write!(self.log_file, "{:X} ", opcode).unwrap();
 
         match opcode {
+            0x18 => {
+                write!(self.log_file, "CLC").unwrap();
+                Ok(self.clc())
+            }
             0x20 => {
                 let operand = self.get_operand_absolute(bus)?;
                 write!(self.log_file, "JSR {}", operand).unwrap();
@@ -55,24 +65,49 @@ impl CPU {
                 write!(self.log_file, "JMP {}", operand).unwrap();
                 Ok(self.jmp(operand)?)
             }
+            0x85 => {
+                let operand = self.get_operand_zeropage(bus)?;
+                write!(self.log_file, "STA {}", operand).unwrap();
+                Ok(self.sta(operand, bus)?)
+            }
             0x86 => {
                 let operand = self.get_operand_zeropage(bus)?;
                 write!(self.log_file, "STX {}", operand).unwrap();
                 Ok(self.stx(operand, bus)?)
+            }
+            0x90 => {
+                let operand = self.get_operand_relative(bus)?;
+                write!(self.log_file, "BCC {}", operand).unwrap();
+                Ok(self.bcc(operand)?)
             }
             0xA2 => {
                 let operand = self.get_operand_immediate(bus)?;
                 write!(self.log_file, "LDX {}", operand).unwrap();
                 Ok(self.ldx(operand)?)
             }
+            0xA9 => {
+                let operand = self.get_operand_immediate(bus)?;
+                write!(self.log_file, "LDA {}", operand).unwrap();
+                Ok(self.lda(operand)?)
+            }
             0xB0 => {
                 let operand = self.get_operand_relative(bus)?;
                 write!(self.log_file, "BCS {}", operand).unwrap();
-                Ok(self.bcs(operand, bus)?)
+                Ok(self.bcs(operand)?)
+            }
+            0xD0 => {
+                let operand = self.get_operand_relative(bus)?;
+                write!(self.log_file, "BNE {}", operand).unwrap();
+                Ok(self.bne(operand)?)
             }
             0xEA => {
                 write!(self.log_file, "NOP").unwrap();
                 Ok(2)
+            }
+            0xF0 => {
+                let operand = self.get_operand_relative(bus)?;
+                write!(self.log_file, "BEQ {}", operand).unwrap();
+                Ok(self.beq(operand)?)
             }
             _ => Err("Invalid opcode"),
         }
@@ -148,10 +183,43 @@ impl CPU {
         }
     }
 
+    fn lda(&mut self, operand: Operand) -> Result<u8, &'static str> {
+        match operand {
+            Operand::IMMEDIATE(val) => {
+                let zero_flag = if val == 0 {
+                    Status::ZERO::SET
+                } else {
+                    Status::ZERO::CLEAR
+                };
+                let negative_flag = if val.bit(7) {
+                    Status::NEGATIVE::SET
+                } else {
+                    Status::NEGATIVE::CLEAR
+                };
+
+                self.registers.accumulator = val;
+                self.registers.status_register.modify(zero_flag);
+                self.registers.status_register.modify(negative_flag);
+                Ok(2)
+            }
+            _ => Err("Unsupported instruction occured"),
+        }
+    }
+
     fn stx<T: Bus>(&mut self, operand: Operand, bus: &mut T) -> Result<u8, &'static str> {
         match operand {
             Operand::ZEROPAGE { operand, .. } => {
                 bus.write_byte(operand as usize, self.registers.x_reg)?;
+                Ok(3)
+            }
+            _ => Err("Unsupported instruction occured"),
+        }
+    }
+
+    fn sta<T: Bus>(&mut self, operand: Operand, bus: &mut T) -> Result<u8, &'static str> {
+        match operand {
+            Operand::ZEROPAGE { operand, .. } => {
+                bus.write_byte(operand as usize, self.registers.accumulator)?;
                 Ok(3)
             }
             _ => Err("Unsupported instruction occured"),
@@ -163,14 +231,46 @@ impl CPU {
         2
     }
 
-    fn bcs<T: Bus>(&mut self, operand: Operand, bus: &mut T) -> Result<u8, &'static str> {
+    fn clc(&mut self) -> u8 {
+        self.registers.status_register.modify(Status::CARRY::CLEAR);
+        2
+    }
+
+    fn bcs(&mut self, operand: Operand) -> Result<u8, &'static str> {
+        self.branchif(operand, true, Status::CARRY)
+    }
+
+    fn bcc(&mut self, operand: Operand) -> Result<u8, &'static str> {
+        self.branchif(operand, false, Status::CARRY)
+    }
+
+    fn beq(&mut self, operand: Operand) -> Result<u8, &'static str> {
+        self.branchif(operand, true, Status::ZERO)
+    }
+
+    fn bne(&mut self, operand: Operand) -> Result<u8, &'static str> {
+        self.branchif(operand, false, Status::ZERO)
+    }
+
+    fn branchif(
+        &mut self,
+        operand: Operand,
+        set: bool,
+        flag: Field<u8, Register>,
+    ) -> Result<u8, &'static str> {
         match operand {
             Operand::RELATIVE {
                 operand,
                 program_counter,
             } => {
+                let check = if set {
+                    self.registers.status_register.is_set(flag)
+                } else {
+                    !self.registers.status_register.is_set(flag)
+                };
+
                 let mut cycle_count = 2;
-                if self.registers.status_register.is_set(Status::CARRY) {
+                if check {
                     cycle_count += 1;
                     let new_addr = operand as usize + program_counter;
 
